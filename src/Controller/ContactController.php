f7d8eee580f5aa4355a800797236c8f84b7d1107
Koched92 <hichem.koched@gmail.com>
2020-10-13 20:09:12 +0200

    SwiftMailer Bundle

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -10,10 +12,38 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
+
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $contact = $form->getData();
+
+            //envoi de mail
+            $message = (new \Swift_Message('Nouveau Contact'))
+
+            //on attribue l'expéditeur
+            ->setFrom($contact['mail'])
+
+            //on attribue le destinataire
+            ->setTo('hichem.koched@gmail.com')
+
+            //on crée le message avec la vue twig
+            ->setBody(
+                $this->renderView('emails/contactMail.html.twig', compact('contact')
+                ),
+                'text/html'
+            );
+            // on envoie le message
+            $mailer->send($message);
+
+            $this->addFlash('success', 'Le message a bien été envoyé');
+            return $this->redirectToRoute('contact');
+
+        }
         return $this->render('contact/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            'contactForm' =>  $form->createView(),
         ]);
     }
 }
